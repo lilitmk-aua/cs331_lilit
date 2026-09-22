64e36ac62797eef4459aa128e4e09f59f7be0d89
lilitmk-aua <lilit_mkhitaryan@edu.aua.am>
2026-09-22 16:17:45 +0000

    homework 4

---
+++ homeworks/hw4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(){
+
+    int a = 12;
+    printf("the square of %d is %d \n", a, square(a));
+
+    return 0;
+}
---
+++ homeworks/hw4/math_utils.c
@@ -0,0 +1,5 @@
+#include "math_utils.h"
+
+int square(int i){
+    return i*i;
+}
